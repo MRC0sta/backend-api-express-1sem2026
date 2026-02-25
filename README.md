84ffe643bd8c31ddbfbfd2a4c2cb5506c814ad28
Matheus Costa <matheusrc0107@gmail.com>
2026-02-24 22:10:24 -0300

    teste de métodos http

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # Tópicos Importantes
 
 - Qual a linguagem de programação utilizada para o nosso Backend?
-- o que é Node.js?
-- o que é NPM?
-- o que é Express.js?
+- O que é Node.js?
+- O que é NPM?
+- O que é Express.js?
 - Como mudar o formato de importação de "require" (padrão common.js) para module -> package.json e mudar para: "type": "module"
+- O que é HTTP?
+- Quais os métodos de requisição HTTP e para que serve cada um deles?
--- src/server.js
+++ src/server.js
@@ -9,7 +9,23 @@ app.get('/', (req, res) => {
 })
 
 app.get('/user', (req, res) => {
-  res.send('Você está no user')
+  res.send('Get na rota /user')
+})
+
+app.post('/user', (req, res) => {
+  res.send('Post na rota /user')
+})
+
+app.patch('/user', (req, res) => {
+  res.send('Patch na rota /user')
+})
+
+app.put('/user', (req, res) => {
+  res.send('Put na rota /user')
+})
+
+app.delete('/user', (req, res) => {
+  res.send('Delete na rota /user')
 })
 
 app.listen(port, () => {
