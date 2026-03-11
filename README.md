197680471f8b868ca37121e0efe22a23bbf00a20
Matheus Costa <matheusrc0107@gmail.com>
2026-03-10 22:27:37 -0300

    add model create user with prisma

--- README.md
+++ README.md
@@ -11,3 +11,18 @@
 - O que é um padrão de projeto?
 - O que é o padrão de projeto MVC?
 - Qual a responsabilidade de cada Camada?
+- O que é um ORM?
+- Quais as vantagens de utilizar um ORM?
+
+### Comandos prisma
+
+Instalacoes
+- npm i prisma -D
+- npm i dotenv -D
+
+Mysql e Mariadb
+
+Init
+npx prisma init --datasource-provider mysql --output ../generated/prisma/prisma
+
+npm install @prisma/client @prisma/adapter-mariadb mariadb
--- src/controllers/user/createUsersController.js
+++ src/controllers/user/createUsersController.js
@@ -1,3 +1,18 @@
-export function createUsersController(req, res){
-	res.send('Post na rota /user respondida pelo Controller')
+import { createUser } from "../../models/userModel.js"
+
+export async function createUsersController(req, res) {
+
+	const user = {
+		avatar: "https://github.com/MRC0sta.png",
+		name: "Matheus Costa",
+		email: "matheusrc0107@gmail.com",
+		pass: "12345678"
+	};
+
+	const result = await createUser(user)
+
+	res.json({
+		message: "Usuário criado com sucesso!",
+		user: result
+	})
 }
